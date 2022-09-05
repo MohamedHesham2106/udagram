8b05771bb407d01f53bda80e0c8d8177dd31dc55
Mohamed Hesham <moohisham2000@gmail.com>
2022-09-05 12:38:14 +0200

    chore
    package.json

--- udagram/udagram-frontend/bin/deploy.sh
+++ udagram/udagram-frontend/bin/deploy.sh
@@ -1,15 +1,15 @@
 aws s3 cp --recursive --acl public-read ./www s3://raidenshuriken-udagram/
 aws s3 cp --acl public-read --cache-control="max-age=0, no-cache, no-store, must-revalidate" ./www/index.html s3://raidenshuriken-udagram/
-eb setenv AWS_ACCESS_KEY_ID=AKIAWS5QR4K4X7LKM2SF 
-eb setenv AWS_BUCKET=raidenshuriken-udagram 
+eb setenv AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID
+eb setenv AWS_BUCKET=$AWS_BUCKET
 eb setenv AWS_PROFILE=default 
-eb setenv AWS_SECRET_ACCESS_KEY=cHJkUe1pkZVcpb7JnE49QtnWIr/gsVsUd6Frqbu+
-eb setenv DB_PORT=5432 
-eb setenv JWT_SECRET=your-secret-token 
-eb setenv PORT=3000 
-eb setenv POSTGRES_DB=postgres
-eb setenv POSTGRES_HOST=database-1.cw2fhwwzswea.us-east-1.rds.amazonaws.com
-eb setenv POSTGRES_PASSWORD=raidenshuriken
-eb setenv POSTGRES_USERNAME=postgres   
-eb setenv RDS_DIALECT=postgres  
-eb setenv URL=http://raidenshuriken-udagram.s3-website-us-east-1.amazonaws.com
+eb setenv AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY
+eb setenv DB_PORT=$DB_PORT
+eb setenv JWT_SECRET=$JWT_SECRET 
+eb setenv PORT=$PORT 
+eb setenv POSTGRES_DB=$POSTGRES_DB
+eb setenv POSTGRES_HOST=$POSTGRES_HOST
+eb setenv POSTGRES_PASSWORD=$POSTGRES_PASSWORD
+eb setenv POSTGRES_USERNAME=$POSTGRES_USERNAME   
+eb setenv RDS_DIALECT=$RDS_DIALECT  
+eb setenv URL=$URL
